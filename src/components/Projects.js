1b9a99d703e8695ea835deb678ba7315334b9935
vaibhav Dwivedi <vaibhavdwivedi3212gmail.com>
2023-05-22 16:33:34 +0530

    moreproj

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,8 @@ import projImg9 from "../assets/img/proj9.png";
 import projImg10 from "../assets/img/proj10.png";
 import projImg11 from "../assets/img/proj11.png";
 import projImg12 from "../assets/img/proj12.png";
+import projImg13 from "../assets/img/projnew1.png";
+import projImg14 from "../assets/img/projnew2.png"
 
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
@@ -20,6 +22,18 @@ import TrackVisibility from 'react-on-screen';
 export const Projects = () => {
 
   const projects = [
+    {
+      title: "VODEX2.0",
+      description: "My own personalized chatGPT with ability to provide MCQ answer more effieciently.",
+      imgUrl: projImg13,
+      linked: "https://vodex-vd.vercel.app/"
+    },
+    {
+      title: "VChat",
+      description: "A microsoftTeams clone made for chatting.",
+      imgUrl:projImg14,
+      linked: "https://vaibhav-vchat.onrender.com"
+    },
     {
       title: "CORPTALKS",
       description: "A feedback giver made for corporations",
@@ -44,6 +58,11 @@ export const Projects = () => {
       imgUrl: projImg4,
       linked: "https://vaibhavdwivedi-321.github.io/SimonGame-Memorize/"
     },
+    
+  ];
+
+
+  const projects2 = [
     {
       title: "Drum Kit",
       description: "A Drum Kit made using JavaScript!",
@@ -56,10 +75,6 @@ export const Projects = () => {
       imgUrl: projImg6,
       linked: "https://vaibhavdwivedi-321.github.io/NotesKeeper/"
     },
-  ];
-
-
-  const projects2 = [
     {
       title: "DiceGame",
       description: "A new way to declare the winner of the toss",
@@ -78,27 +93,29 @@ export const Projects = () => {
       imgUrl: projImg9,
       linked: "https://newslettersignup-vd.onrender.com/"
     },
-    {
-      title: "TodoList React",
-      description: "A simple TodoList made using React.js",
-      imgUrl: projImg10,
-      linked: "https://vaibhavdwivedi-321.github.io/React.js-To-Do-List/"
-    },
+
     {
       title: "Tindog",
       description: "A website for Dogs made using Bootstrap",
       imgUrl: projImg11,
       linked: "https://vaibhavdwivedi-321.github.io/Tindog-TinderForDogs/"
-    },
-    {
-      title: "Personal Site",
-      description: "My first HTML Project",
-      imgUrl: projImg12,
-      linked: "https://vaibhavdwivedi-321.github.io/HTMLPersonalSite/"
     }
 
   ];
-
+const projects3 = [
+  {
+    title: "Personal Site",
+    description: "My first HTML Project",
+    imgUrl: projImg12,
+    linked: "https://vaibhavdwivedi-321.github.io/HTMLPersonalSite/"
+  },
+  {
+    title: "TodoList React",
+    description: "A simple TodoList made using React.js",
+    imgUrl: projImg10,
+    linked: "https://vaibhavdwivedi-321.github.io/React.js-To-Do-List/"
+  }
+];
 
 
 
@@ -126,8 +143,11 @@ export const Projects = () => {
                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">My Resume</Nav.Link>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
                     </Nav.Item>
+                    {/* <Nav.Item>
+                      <Nav.Link eventKey="third">My Resume</Nav.Link>
+                    </Nav.Item> */}
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
@@ -160,9 +180,25 @@ export const Projects = () => {
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
+
+                      <Row>
+                        {
+                          projects3.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    
+                    {/* <Tab.Pane eventKey="third">
                       <p><a href="https://drive.google.com/file/d/1e00JhzZq82BzffwxRjbk9gqkf1YHGUZt/view?usp=share_link">My Resume</a></p>
 
-                    </Tab.Pane>
+                    </Tab.Pane> */}
                   </Tab.Content>
                 </Tab.Container>
               </div>}
